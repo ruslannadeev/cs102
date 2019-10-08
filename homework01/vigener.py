e3f03a9b400286955fb29c98470d1e341e10ba87
ruslannadeev <55666463+ruslannadeev@users.noreply.github.com>
2019-10-08 14:08:55 +0300

    вижинир настроен

--- homework01/vigener.py
+++ homework01/vigener.py
@@ -1,26 +1,42 @@
-def encrypt(plaintext, key):
-    difference = len(plaintext) // len(key)
-    key *= difference + 1
+def encrypt_vigenere(plaintext, keyword):
+    """
+    Encrypts plaintext using a Vigenere cipher.
+
+    >>> encrypt_vigenere("PYTHON", "A")
+    'PYTHON'
+    >>> encrypt_vigenere("python", "a")
+    'python'
+    >>> encrypt_vigenere("ATTACKATDAWN", "LEMON")
+    'LXFOPVEFRNHR'
+    """
+    
+    difference = len(plaintext) // len(keyword)
+    keyword *= difference + 1
     ciphertext = ''
     for index, letter in enumerate(plaintext):
-        replaced = (ord(letter) + ord(key[index]))
+        replaced = (ord(letter) + ord(keyword[index]))
         encrypted  = chr(replaced % 26 + 65)
         ciphertext += encrypted
     return(ciphertext)
 
-def decrypt(ciphertext, key):
-    difference = len(ciphertext) // len(key)
-    key *= difference + 1
+def decrypt_vigenere(ciphertext, keyword):
+    """
+    Decrypts a ciphertext using a Vigenere cipher.
+
+    >>> decrypt_vigenere("PYTHON", "A")
+    'PYTHON'
+    >>> decrypt_vigenere("python", "a")
+    'python'
+    >>> decrypt_vigenere("LXFOPVEFRNHR", "LEMON")
+    'ATTACKATDAWN'
+    """
+    
+    difference = len(ciphertext) // len(keyword)
+    keyword *= difference + 1
     plaintext = ''
     for index, letter in enumerate(ciphertext):
-        replaced_back = (ord(letter) - ord(key[index]))
+        replaced_back = (ord(letter) - ord(keyword[index]))
         decrypted = chr(replaced_back % 26 + ord('A'))
         plaintext += decrypted
     return(plaintext)
 
-plaintext = input('Enter a message: ').upper()
-key = input('Enter a key : ').upper()
-encrypted_msg = encrypt(plaintext, key)
-decrypted_msg = decrypt(encrypted_msg, key)
-print(encrypted_msg)
-print(decrypted_msg)
